e85c2375d572d2404319c79ff218bce6d21f9565
Eben <96250954+EbenJesusSaves@users.noreply.github.com>
2024-06-02 03:04:01 +0000

    started routing

--- Src/server/app.ts
+++ Src/server/app.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import router from "../Routers/router";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "this this this this" });
+});
+
 export default app;
+
+app.use("/user", router);
