7f2bde86d7b66840f30e4a3456c4432d2e2b72ee
Eben <96250954+EbenJesusSaves@users.noreply.github.com>
2024-06-20 22:02:38 +0000

    added draft amd post endpoint updates

--- Src/Routers/router.ts
+++ Src/Routers/router.ts
@@ -1,7 +1,29 @@
 import { Router } from "express";
-import { postContent } from "../handlers/routers";
+import {
+  deleteComment,
+  deleteDraft,
+  deletePost,
+  editDraftPost,
+  editPost,
+  getDraftPosts,
+  postContent,
+  postDraftPost,
+  searchPost,
+} from "../handlers/routers";
 
 const router = Router();
 
 router.post("/post", postContent);
+router.delete("/delete_post", deletePost);
+router.put("/edit_post", editPost);
+router.post("/search_post", searchPost);
+
+router.delete("/delete_comment", deleteComment);
+
+//============ draft posts ===================//
+router.get("/draft_post", getDraftPosts);
+router.post("/draft_post", postDraftPost);
+router.delete("/delete_draft_post", deleteDraft);
+router.put("/edit_draft_post", editDraftPost);
+
 export default router;
